9f5584b114249fef1a1dac0c839cd0bc7bb217a2
longmaker2 <l.deng@alustudent.com>
2023-02-17 23:30:39 +0200

    updated

--- business_web/script.js
+++ business_web/script.js
@@ -1,9 +1,74 @@
-// Select all the "Enroll Now" buttons on the page
-var enrollButtons = document.querySelectorAll('.button');
+// Navigation toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
 
-// Add a click event listener to each button
-enrollButtons.forEach(function(button) {
-	button.addEventListener('click', function() {
-		alert('Thank you for enrolling in this course!');
-	});
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
 });
+
+// Scroll to top button
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+// Form validation
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Check for name input
+  if (nameInput.value === '') {
+    setErrorFor(nameInput, 'Name is required');
+  } else {
+    setSuccessFor(nameInput);
+  }
+
+  // Check for email input
+  if (emailInput.value === '') {
+    setErrorFor(emailInput, 'Email is required');
+  } else if (!isValidEmail(emailInput.value)) {
+    setErrorFor(emailInput, 'Email is not valid');
+  } else {
+    setSuccessFor(emailInput);
+  }
+
+  // Check for message input
+  if (messageInput.value === '') {
+    setErrorFor(messageInput, 'Message is required');
+  } else {
+    setSuccessFor(messageInput);
+  }
+});
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const error = formControl.querySelector('.error-message');
+  formControl.classList.add('error');
+  error.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.classList.remove('error');
+}
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
