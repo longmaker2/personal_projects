c99253ac04d0a308d85f85bee9e0a01873eaa450
longmaker2 <l.deng@alustudent.com>
2023-03-08 03:37:41 +0200

    file updated

--- python_app/main.py
+++ python_app/main.py
@@ -1,51 +1,63 @@
-import kivy
+from kivy.config import Config
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '550')
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.core.window import Window
 
 class PatientRecordsApp(App):
     def build(self):
-        layout = GridLayout(cols=2)
+        # Set background color
+        Window.clearcolor = (0.3, 0.3, 0.3, 1)
 
-        # Add labels and text input widgets for each field in the patient record
-        layout.add_widget(Label(text='Patient Name'))
-        self.patient_name = TextInput()
-        layout.add_widget(self.patient_name)
+        # Create custom title
+        title_layout = GridLayout(cols=1, size_hint=(1, None), height=100)
+        title_label = Label(text='Patient Records', font_size=36)
+        title_layout.add_widget(title_label)
 
-        layout.add_widget(Label(text='Patient ID'))
-        self.patient_id = TextInput()
-        layout.add_widget(self.patient_id)
+        # Create main layout
+        main_layout = GridLayout(cols=2, spacing=10, padding=30)
+        main_layout.add_widget(Label(text='Patient Name'))
+        self.patient_name = TextInput(multiline=False)
+        main_layout.add_widget(self.patient_name)
 
-        layout.add_widget(Label(text='Doctor Name'))
-        self.doctor_name = TextInput()
-        layout.add_widget(self.doctor_name)
+        main_layout.add_widget(Label(text='Patient ID'))
+        self.patient_id = TextInput(multiline=False)
+        main_layout.add_widget(self.patient_id)
 
-        layout.add_widget(Label(text='Symptoms'))
-        self.symptoms = TextInput()
-        layout.add_widget(self.symptoms)
+        main_layout.add_widget(Label(text='Doctor Name'))
+        self.doctor_name = TextInput(multiline=False)
+        main_layout.add_widget(self.doctor_name)
 
-        layout.add_widget(Label(text='Diagnosis'))
-        self.diagnosis = TextInput()
-        layout.add_widget(self.diagnosis)
+        main_layout.add_widget(Label(text='Symptoms'))
+        self.symptoms = TextInput(multiline=True)
+        main_layout.add_widget(self.symptoms)
 
-        layout.add_widget(Label(text='Prescription'))
-        self.prescription = TextInput()
-        layout.add_widget(self.prescription)
+        main_layout.add_widget(Label(text='Diagnosis'))
+        self.diagnosis = TextInput(multiline=True)
+        main_layout.add_widget(self.diagnosis)
 
-        # Add buttons for saving and retrieving patient records
-        save_button = Button(text='Save')
-        save_button.bind(on_press=self.save_record)
-        layout.add_widget(save_button)
+        main_layout.add_widget(Label(text='Prescription'))
+        self.prescription = TextInput(multiline=True)
+        main_layout.add_widget(self.prescription)
 
-        get_button = Button(text='Get Records')
-        get_button.bind(on_press=self.get_records)
-        layout.add_widget(get_button)
+        # Create buttons layout
+        buttons_layout = GridLayout(cols=2, size_hint_y=None, height=50)
+        buttons_layout.add_widget(Button(text='Save', on_press=self.save_record))
+        buttons_layout.add_widget(Button(text='Get Records', on_press=self.get_records))
 
-        return layout
+        # Combine all layouts into one
+        root_layout = GridLayout(cols=1)
+        root_layout.add_widget(title_layout)
+        root_layout.add_widget(main_layout)
+        root_layout.add_widget(buttons_layout)
 
-    def save_record(self):
+        return root_layout
+
+    def save_record(self, instance):
         print(f"Patient Name: {self.patient_name.text}")
         print(f"Patient ID: {self.patient_id.text}")
         print(f"Doctor Name: {self.doctor_name.text}")
@@ -53,6 +65,9 @@ class PatientRecordsApp(App):
         print(f"Diagnosis: {self.diagnosis.text}")
         print(f"Prescription: {self.prescription.text}")
 
-    def get_records(self):
+    def get_records(self, instance):
         # TODO: Implement method for retrieving patient records from a database or file
         pass
+
+if __name__ == '__main__':
+    PatientRecordsApp().run()
